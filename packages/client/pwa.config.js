84a7457ba8835eec7cdd054c0b337814f8fb7c6a
olso <olsansky.martin@gmail.com>
2018-09-11 21:42:37 +0200

    recursive install

--- packages/client/pwa.config.js
+++ packages/client/pwa.config.js
@@ -1,8 +1,6 @@
 const { join } = require('path');
 
 exports.webpack = function(config, env) {
-  const { production, webpack } = env;
-
   config.resolve.alias = {
     ...config.resolve.alias,
     '@stores': join(env.src, 'stores'),
