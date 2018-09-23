c698158fa670050b46bb3d72e135fc5e10a52fef
olso <olsansky.martin@gmail.com>
2018-09-23 13:44:15 +0200

    hydratenewuser

--- packages/serverless/firebase/functions/src/index.ts
+++ packages/serverless/firebase/functions/src/index.ts
@@ -1,5 +1,26 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+import { IUserData } from "../../../../common/types/User";
+
+exports.hydrateNewUser = functions.auth.user().onCreate(async user => {
+  const newUserData: IUserData = {
+    payments: [],
+    purchases: [],
+    accessLog: [],
+    credits: 0,
+    note: "",
+    role: "unassigned",
+    parentId: null
+  };
+
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .set(newUserData);
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
